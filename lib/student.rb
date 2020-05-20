c9245e64b78b45b181004e85f5bee0f41a510065
Anna Guerrero <acg2176@columbia.edu>
2020-05-20 05:32:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash.values[0]
+    @location = student_hash.values[1]
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) #output of scraper.scrape_index_page, {name => "a", location => "b", profile_url => "c"}
+    student_hash = {}
+    students_array.each do |student| #list of dictionaries
+      student_hash[:name] = student[:name]#hash
+      student_hash[:location] = student[:location]
+    end
+    student_hash
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
